0b1866727f097ddc77b7036a718bb7ab59e4eddf
Nate Tan <nktan93@gmail.com>
2019-05-09 19:44:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,5 +25,11 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 end
