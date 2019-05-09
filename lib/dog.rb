98971bf9936349629355f042f52da4bf1273453f
Nate Tan <nktan93@gmail.com>
2019-05-09 20:12:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,7 @@ class Dog
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    Song.new(result[0], result[1], result[2])
+    self.new(result[0], result[1], result[2])
   end
   
   def self.new_from_db(row)
