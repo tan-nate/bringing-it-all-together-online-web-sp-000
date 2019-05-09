50783d33aa68b8e7109ac9be5b8c2003e2788f93
Nate Tan <nktan93@gmail.com>
2019-05-09 19:56:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,7 @@ class Dog
       SQL
  
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
   
